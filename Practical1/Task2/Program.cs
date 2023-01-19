23bd83ca3966ca5cfbf093f7c2eaa790cbbeb6a1
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-01-19 23:59:41 +0300

    The second task.

---
+++ Practical1/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter the first number: ");
+string? firstNumber = Console.ReadLine();
+int a = Int32.Parse(firstNumber);
+
+Console.WriteLine("Enter the second number: ");
+string? secondNumber = Console.ReadLine();
+int b = Int32.Parse(secondNumber);
+
+int max, min = 0;
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine("{0} - max number, {1} - min number", max, min);

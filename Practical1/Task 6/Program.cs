6207621a404fce76703f59c4b3b384d19940d742
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-01-20 03:30:21 +0300

    Task 6.

---
+++ Practical1/Task 6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter the number: ");
+string? firstNumber = Console.ReadLine();
+int a = Int32.Parse(firstNumber);
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("a - even number");
+}
+else
+{
+    Console.WriteLine("a - odd number");
+}

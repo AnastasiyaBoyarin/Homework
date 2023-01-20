b55c552b70499e36869ffec239c200dc9e933d72
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-01-20 03:38:15 +0300

    Task 8.

---
+++ Practical1/Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter the number: ");
+string? Number = Console.ReadLine();
+int a = Int32.Parse(Number);
+
+for (int i = 1; i <= a; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine("even number - {0}", i);
+    }
+}

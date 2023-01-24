7629c4fbc6878d7841787dff436b14465d59a52a
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-01-24 15:43:01 +0300

    Task 15

---
+++ Practical1/Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter number of day on the week (1-7):");
+int a = Int32.Parse(Console.ReadLine());
+
+if (a == 6 || a == 7)
+{
+    Console.WriteLine("You entered number of day is day off - YES");
+}
+else
+{
+    Console.WriteLine("You entered number of day is weekday - NO");
+}

a38bacaf91859856bff820c57e6a8d10f250868d
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-01-24 15:32:13 +0300

    Task 10

---
+++ Practical1/Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter three-digit number: ");
+int a = Int32.Parse(Console.ReadLine());
+
+string str = a.ToString();
+int[] b = new int[str.Length];
+for(int i=0; i< str.Length; i++)
+{
+     b[i] = int.Parse(str[i].ToString());
+}
+
+Console.WriteLine("{0} -> {1}", a, b[1]);

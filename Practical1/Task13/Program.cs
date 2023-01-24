f58f0e12d98d84513f8d56538b79b6201a26906e
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-01-24 15:38:18 +0300

    Task 13

---
+++ Practical1/Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter number: ");
+int a = Int32.Parse(Console.ReadLine());
+
+string str = a.ToString();
+int[] b = new int[str.Length];
+for(int i=0; i< str.Length; i++)
+{
+     b[i] = int.Parse(str[i].ToString());
+}
+
+if (b.Length >= 3)
+{
+    Console.WriteLine("The third digit of number: {0}", b[2]);
+}
+else
+{
+    Console.WriteLine("You enter not three-digit number");
+}

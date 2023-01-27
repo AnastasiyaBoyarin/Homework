9a1aee6bf2bd3b5e0acd35963acaebbfe00dd441
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-01-27 20:06:05 +0300

    Task23

---
+++ Practical1/Task23/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter number N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write("Number is : {0} and cube of the {1} is : {2} \n", i, i,(i*i*i));
+}

018bf30f64b26b6eeab620a3adde7c3c4df0cc76
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-01-27 19:40:25 +0300

    Task19

---
+++ Practical1/Task19/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Enter five digit number");
+int a = Int32.Parse(Console.ReadLine());
+
+int reverse = 0;
+int n = a;
+
+if (a.ToString().Length != 5)
+{
+    Console.WriteLine("You enter not five digit number. Could you please try again");
+    return;
+}
+
+while (n > 0)
+{
+    int r = n % 10;
+    reverse = reverse * 10 + r;
+    n = n / 10;
+}
+
+if (a == reverse)
+{
+    Console.WriteLine("Entered five digit number is palidrom");
+}
+else
+{
+    Console.WriteLine("Entered five digit number is not palidrom");
+}

a38f38702d5dc2542690d824b3ce46f413c84cce
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-01-29 19:31:29 +0300

    Task27

---
+++ Practical1/Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter number:");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = GetSum(number);
+
+Console.WriteLine("{0} -> {1}", number, result);
+
+int GetSum(int number)
+{
+    int m, sum = 0;
+    while (number > 0)      
+    {      
+        m = number % 10;      
+        sum = sum + m;      
+        number = number / 10;      
+    }
+    return sum; 
+}

2ea76c552592ea74a0870a32bc8c17bbe9a558b7
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-01-29 19:43:03 +0300

    Task29

---
+++ Practical1/Task29/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Displaying of array which consists of 8 elements:");
+
+int[] array = new int[8];
+Random randNum = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = randNum.Next(0, 100);
+}
+
+DisplayNumbers(array);
+DisplayArray(array);
+
+void DisplayNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    { 
+        if (i == array.Length - 1)
+        {
+            Console.Write("{0}", array[i]);
+        }
+        else
+        {
+            Console.Write("{0}, ", array[i]);
+        }
+    }
+    Console.Write(" -> ");
+}
+
+void DisplayArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    { 
+        if (i == array.Length - 1)
+        {
+            Console.Write("{0}", array[i]);
+        }
+        else
+        {
+            Console.Write("{0}, ", array[i]);
+        }
+    }
+    Console.Write("]");
+}

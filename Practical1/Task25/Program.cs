553ddb64d92662f035a7f647045ae93855c592a2
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-01-29 19:24:53 +0300

    Task25

---
+++ Practical1/Task25/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter two numbers:");
+
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = GetPow(firstNumber, secondNumber);
+
+Console.WriteLine("{0}, {1} -> {2}", firstNumber, secondNumber, result);
+
+int GetPow(int number, int exponent)
+{
+    int i = 1;
+    int power = 1;
+    while (i <= exponent)
+    {
+        power = power * number;
+        i++;
+    }
+    return power;
+}

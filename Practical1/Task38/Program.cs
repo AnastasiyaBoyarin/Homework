0855a767ad2df8c2ca9570e7ae38c8b120c1980d
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-02-03 20:07:44 +0300

    Task38

---
+++ Practical1/Task38/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Displaying of array:");
+
+int[] array = new int[5];
+Random randNum = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = randNum.Next(0, 1000);
+}
+
+DisplayDiffBeetweenMinMaxNumbers(array);
+
+void DisplayDiffBeetweenMinMaxNumbers(int[] array)
+{
+    int min = array[0];
+    int max = array[0];
+    int diff = 0;
+    for (int i = 0; i < array.Length; i++)
+    { 
+        if (array[i] > max)
+        {
+            max = array[i];    
+        }
+
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+
+        if (i == array.Length - 1)
+        {
+            Console.Write("{0}", array[i]);
+        }
+        else
+        {
+            Console.Write("{0}, ", array[i]);
+        }
+    }
+    diff = max - min;
+    Console.Write(" -> {0}", diff);
+}

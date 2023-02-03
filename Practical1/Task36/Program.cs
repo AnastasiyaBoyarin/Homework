b1fb4442607f649d7e1e63771f794a313c402348
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-02-03 20:01:18 +0300

    Task36

---
+++ Practical1/Task36/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Displaying of array:");
+
+int[] array = new int[4];
+Random randNum = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = randNum.Next(0, 1000);
+}
+
+DisplaySumOfOddNumbers(array);
+
+void DisplaySumOfOddNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    { 
+        if (i % 2 == 1)
+        {
+            sum = sum + array[i];
+        }
+        if (i == array.Length - 1)
+        {
+            Console.Write("{0}", array[i]);
+        }
+        else
+        {
+            Console.Write("{0}, ", array[i]);
+        }
+    }
+    Console.Write(" -> {0}", sum);
+}

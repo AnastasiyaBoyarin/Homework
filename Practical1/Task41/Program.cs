8309ffbcb33bdf434e7b2c0487a49e390506e6ab
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-02-06 16:14:42 +0300

    Task41

---
+++ Practical1/Task41/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter count of numbers M:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[m];
+int count = 0;
+
+Console.WriteLine("Enter {0} numbers:", m);
+for (int i = 0; i < m; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine("Count of numbers > 0 -> {0}", count);

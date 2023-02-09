54abdfd1636f5535d83d0ae68c09632842f93365
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-02-09 17:08:05 +0100

    Task47

---
+++ Practical1/Task47/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter size of two-dimensional array");
+
+Console.WriteLine("Enter m:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+Random rnd = new Random();
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = rnd.Next(1, 10);
+        Console.WriteLine("[{0}, {1}] = {2}", i, j, array[i,j]);
+    }
+}

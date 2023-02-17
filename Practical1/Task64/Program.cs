c3a0ef882f173805e29b4115e0ada45908e19cb0
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-02-17 16:25:36 +0100

    Task64

---
+++ Practical1/Task64/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("\n\n Recursion : Print the natural numbers from N to 1 :\n");
+Console.Write("--------------------------------------------------------\n");
+int n = EnterNumber();
+PrintNumbers(n, 1);
+
+int EnterNumber()
+{
+    Console.Write(" Enter number N: ");
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int PrintNumbers(int n, int m)
+{
+    if (n < 1)
+	{
+	    return m;
+	}
+
+	Console.Write(" {0} ", n);
+	n--;
+	return PrintNumbers(n, m);
+}

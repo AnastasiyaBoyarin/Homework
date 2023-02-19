d002890a68180f3919808e0c7ccc798b3ac52054
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-02-19 22:17:44 +0100

    Task58

---
+++ Practical1/Task58/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.WriteLine("Generating two matrices:");
+
+Console.WriteLine("Matrix A:");
+int[,] matrixA = GeneratingArray();
+
+Console.WriteLine("=========================");
+
+Console.WriteLine("Matrix B:");
+int[,] matrixB = GeneratingArray();
+
+Console.WriteLine("=========================");
+
+MatrixMultiplication(matrixA, matrixB);
+
+int[,] GeneratingArray()
+{
+    Random rnd = new Random();
+
+    int[,] resultArray = new int[2, 2];
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            resultArray[i, j] = rnd.Next(1, 10);
+            Console.Write("{0}  ", resultArray[i, j]);
+        }
+        Console.WriteLine();
+    }
+
+    return resultArray;
+}
+
+void MatrixMultiplication(int[,] arrayA, int[,] arrayB)
+{
+    int[,] resultMatrix = new int[2, 2];
+    for (int i = 0; i < 2; i++) 
+    {
+        for (int j = 0; j < 2; j++) 
+        {
+            resultMatrix[i, j] = 0;
+            for (int k = 0; k < 2; k++) 
+            {
+                resultMatrix[i, j] += arrayA[i, k] * arrayB[k, j];
+            }
+        }
+    }
+    Console.WriteLine("The multiplication of the two matrices is:");
+    for (int i = 0; i < 2; i++) 
+    {
+        for (int j = 0; j < 2; j++) 
+        {
+            Console.Write("{0}  ", resultMatrix[i, j]);
+        }
+        Console.WriteLine();
+    }
+}

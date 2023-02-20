5801844e662051fe76bcc177370e50e7d5ba7070
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-02-20 21:49:56 +0100

    Task60

---
+++ Practical1/Task60/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Generating three dimensial array:");
+
+int[,,] array = GeneratingArray();
+
+int[,,] GeneratingArray()
+{
+    Random rnd = new Random();
+
+    int[,,] resultArray = new int[2, 2, 2];
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            for (int k = 0; k < 2; k++)
+            {
+                resultArray[i, j, k] = rnd.Next(10, 100);
+                Console.Write("{3} ({0},{1},{2}) ", i, j, k, resultArray[i, j, k]);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+    return resultArray;
+}
